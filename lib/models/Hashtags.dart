b92bc39922d953ea8b68c5fe161e5c52a8514d25
piratelicorne <thecodeloops@gmail.com>
2020-08-02 02:36:13 -0500

    creating list of pools, posts and hashtags

---
+++ lib/models/Hashtags.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class Hashtag {
+  final String hashtag, user, location;
+  final int id;
+  final Color color;
+  Hashtag({
+    this.id,
+    this.hashtag,
+    this.user,
+    this.location,
+    this.color,
+  });
+}
+
+List< Hashtag> hashtags = [
+   Hashtag(
+    id: 1,
+    hashtag: "#cats",
+    user: "Edith",
+    location: "Dumbarton Circle 74000",
+    color: Colors.deepPurple[50],
+  ),
+  Hashtag(
+    id: 2,
+    hashtag: "#coding",
+    user: "Aline",
+    location: "Dumbarton Circle 74000",
+    color: Colors.deepPurple[50],
+  ),
+  Hashtag(
+    id: 3,
+    hashtag: "#hacking",
+    user: "Meli",
+    location: "Dumbarton Circle 74000",
+    color: Colors.deepPurple[50],
+  ),
+];
