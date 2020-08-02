f900198f8c4cdc0ed0281bb17af5830d4da7cbf9
alineayumi <alineayumi@gmail.com>
2020-08-02 10:42:44 -0300

    recovering

---
+++ lib/models/message_model.dart
@@ -0,0 +1,69 @@
+import './user_model.dart';
+
+class Message {
+  final User sender;
+  final String time;
+  final String text;
+
+  Message({
+    this.sender,
+    this.time,
+    this.text,
+  });
+}
+
+//current user
+final User currentUser = User(
+  id: 0,
+  name: 'Current User',
+  imageUrl: 'assets/images/Edi.png',
+);
+
+//Users
+final User meli = User(
+  id: 1,
+  name: 'Meli',
+  imageUrl: 'assets/images/Meli.png',
+);
+
+final User aline = User(
+  id: 2,
+  name: 'Aline',
+  imageUrl: 'assets/images/Aline.png',
+);
+
+// FAVORITE CONTACTS
+List<User> favorites = [meli, aline];
+
+// EXAMPLE RECENT CHATS
+List<Message> chats = [
+  Message(
+    sender: meli,
+    time: '1:30 PM',
+    text: 'Hello, Edi!',
+  ),
+  Message(
+    sender: aline,
+    time: '1:35 PM',
+    text: 'How is everybody?',
+  ),
+];
+
+// EXAMPLE MSGS ON CHAT SCREEN
+List<Message> messages = [
+  Message(
+    sender: meli,
+    time: '1:30 PM',
+    text: 'Hello, Edi!',
+  ),
+  Message(
+    sender: currentUser,
+    time: '1:35 PM',
+    text: 'Hellooooo! :D',
+  ),
+  Message(
+    sender: meli,
+    time: '1:30 PM',
+    text: 'What are you gonna do today?',
+  ),
+];
