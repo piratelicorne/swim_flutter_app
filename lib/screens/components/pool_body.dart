10c26eafacb3c47531e9da02f1486827e50312bb
piratelicorne <thecodeloops@gmail.com>
2020-08-10 03:43:16 -0500

    changes will be added into pool_body

--- lib/screens/components/pool_body.dart
+++ lib/screens/components/pool_body.dart
@@ -35,169 +35,43 @@ class PBSectChat extends StatelessWidget {
   @override
   Widget build(BuildContext context){
     return Container(
+      /**
+       * 
+
+class PCSectChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
       alignment: Alignment.center,
       color: Colors.deepPurple[100].withOpacity(0.4),
-    );
-  }
-}
-
-/*
-class GBSectButtons extends StatelessWidget {
-  @override
-  Widget build(BuildContext context){
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: <Widget>[
-        RaisedButton(
-          child: Text('MEMBERS',
-            style: TextStyle(
-              fontSize: 13.0,
-              //fontFamily: 'NunitoBold',
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget> [
+          Text(
+          'Search',
+          style: TextStyle(
+            color: Colors.deepPurple[600],
+            fontSize: 22,
             ),
           ),
-          color: Colors.deepPurple,
-          textColor: Colors.white,
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => GroupMembersListApp()),
-            );
-          },
-          shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(11.0)),
-        ),
-        RaisedButton(
-          child: Text('INVITE',
-            style: TextStyle(
-              fontSize: 13.0,
-              //fontFamily: 'NunitoBold',
-            ),
-          ),
-          color: Colors.deepPurple,
-          textColor: Colors.white,
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => FriendsListApp()),
-            );
-          },
-          shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(11.0)),
-        ),
-      ],
-    );
-  }
-}
-
-class GBSectMedia extends StatelessWidget {
-  @override
-  Widget build(BuildContext context){
-    return Container(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Column(
-              children: <Widget>[
-                GBSectPools(),
-                GBSectPosts(),
-              ],
-            ),
-            GBSectHashtags(),
-          ]
-        )
-    );
-  }
-}
-
-class GBSectPools extends StatelessWidget {
-  @override
-  Widget build(BuildContext context){
-    return Container(
-      child: Column(
-        children: <Widget>[
-          Text("Pools near you",
-            style: TextStyle(
-              fontSize: 12,
-              //fontFamily: 'NunitoExtraBold',
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.all(8.0),
-            height: 100,
-            width: 120,
-            decoration: BoxDecoration(
-              color: pools[0].color,
-              borderRadius: BorderRadius.circular(16),
-            ),
-            child: Image.asset(pools[0].image),
-          ),
+          IconButton(
+                  icon: Icon(Icons.search),
+                  color: Colors.deepPurple[800],
+                  iconSize: 28,
+                  onPressed: (){},
+                ),
         ],
       ),
+      
+      //iconTheme: IconThemeData(color: Colors.white),
     );
   }
 }
 
-class GBSectPosts extends StatelessWidget {
+class PCSectOptions extends StatelessWidget {
   @override
   Widget build(BuildContext context){
-    return Container(
-      child: Column(
-        children: <Widget>[
-          Text("Recent Posts",
-            style: TextStyle(
-              fontSize: 12,
-              //fontFamily: 'NunitoExtraBold',
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.all(8.0),
-            height: 100,
-            width: 120,
-            decoration: BoxDecoration(
-              color: posts[1].color,
-              borderRadius: BorderRadius.circular(16),
-            ),
-            child: Image.asset(posts[1].image),
-            
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class GBSectHashtags extends StatelessWidget {
-  @override
-  Widget build(BuildContext context){
-    return Container(
-      child: Column(
-        children: <Widget>[
-          Text("Hashtags",
-            style: TextStyle(
-              fontSize: 12,
-              //fontFamily: 'NunitoExtraBold',
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.all(8.0),
-            height: 150,
-            width: 120,
-            decoration: BoxDecoration(
-              color: hashtags[1].color,
-              borderRadius: BorderRadius.circular(16),
-            ),
-            child: Text(hashtags[0].hashtag),
-          ),
-        ]
-      ),
-    );
-  }
-}
-
-
-//PENDING
-
-class GBSectChat extends StatelessWidget {
-  @override
-  Widget build(BuildContext context){
+    /*
     return Container(
       decoration: BoxDecoration(
         color: Colors.teal[900].withOpacity(0.2),
@@ -206,45 +80,34 @@ class GBSectChat extends StatelessWidget {
           topRight: Radius.circular(30.0),
         ),
       ),
-      child: LastMessages(),
-        //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget> [
+          Text(
+          'Chat',
+          style: TextStyle(
+            color: Colors.deepPurple[900],
+            fontSize: 22,
+            ),
+          ),
+        ],
+      ),
+    );
+     
+
+    return MaterialApp(
+      title: 'Chat',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+      ),
+      home: HomeScreen(),
     );
   }
 }
 
-//pending
-
-class LastMessages extends StatelessWidget{
-   @override
-   Widget build(BuildContext context){
-     return Column(
-       children: <Widget>[
-         Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Text(
-                'Last messages',
-                style: TextStyle(
-                  color: Colors.deepPurple[800],
-                 // fontFamily: 'NunitoBold',
-                  fontSize: 18,
-                ),
-              ),
-              IconButton(
-                icon: Icon(Icons.keyboard_arrow_up),
-                color: Colors.deepPurple[800],
-                iconSize: 28,
-                onPressed: (){},
-              ),
-            ],
-          ),
-        ),
-       ],
-     );
-   }
+      */
+      alignment: Alignment.center,
+      color: Colors.deepPurple[100].withOpacity(0.4),
+    );
+  }
 }
-
-class GBSectUser extends StatelessWidget {
-  */
